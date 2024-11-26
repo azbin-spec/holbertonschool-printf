065747abb92a38f904139e0205edfffd42d36041
Bree-Coding <brendapollagba@gmail.com>
2024-11-26 13:38:18 +0100

    add main.h with prototypes

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+
+char_putchar(char character)
+int _printf(const char *format, ...);
+int print_str(const char *size, ...);
+
+
+#endif
