05ceee827d89fc6d1e30d371c18c6b5f59d3a487
root <root@Abdellah.>
2024-11-26 13:36:27 +0100

    creation of _putchar.c

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - prints/writes character 
+ * @character: The character to print 
+ * Return 1, succes, -1 error 
+ */
+
+int _putchar(char character)
+{
+	Return (write(1, &character, 1))
+}		
