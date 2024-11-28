760ac1db046ea1a5cb27bc006a3d73cefa977b68
azbin-spec <dydouhabdellah82@gmail.com>
2024-11-28 09:26:43 +0100

    print_modulo.c modified the code

--- print_modulo.c
+++ print_modulo.c
@@ -1,12 +1,40 @@
 #include "main.h"
 
 /**
- * print_modulo - Fonction that print a modulo character
- * @modulo: a modulo character
+ * _int - print integer.
+ * @args: arguments.
+ *
+ * Return: count chars.
  */
 
-void print_modulo(char modulo)
+int _int(va_list args)
 {
-	modulo = '%';
-	_putchar(modulo);
+	long int num = va_arg(args, int);
+	int numcount = 0;
+	int mul = 1;
+	long int div;
+
+	if (num < 0)
+	{
+		numcount += _putchar('-');
+		num *= -1;
+	}
+
+	if (num < 10)
+		return (numcount += _putchar(num + '0'));
+
+	div = num;
+
+	while (div > 9)
+	{
+		mul *= 10;
+		div /= 10;
+	}
+	while (mul >= 1)
+	{
+		numcount += _putchar(((num / mul) % 10) + '0');
+		mul /= 10;
+	}
+
+	return (numcount);
 }
