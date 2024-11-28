afd49ad2d6d3704f7798fde110f77bccefc40ca3
azbin-spec <dydouhabdellah82@gmail.com>
2024-11-28 09:56:43 +0100

    print_str.c

--- print_str.c
+++ print_str.c
@@ -1,63 +1,25 @@
 #include "main.h"
 
 /**
- * print_str - Fonction that print string,
- * following conversion specifiers %c, %s, %%
- * @format: number of arguments
- * Return: len
+ * print_str - Fonction that print a string
+ * @str: a string
  */
 
-int _printf(const char *format, ...)
+void print_str(const char *str)
 {
-	int x;
-	int y;
-	int len = 0;
-	char *str;
+    int x;
+    const char *n = (null);
 
-	va_list arg;
-	va_start(arg, format);
-
-
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	for (x = 0; format[x] != '\0'; x++)
-	{
-		if (str != NULL)
-		{	
-			str = va_arg(arg, char *);
-
-			if (format[x] == '%' && format[x + 1] == 'c')
-			{
-				_putchar(*str);
-				len++;
-			}
-			else if (format[x] == '%' && format[x + 1] == 's')
-			{
-				for (y = 0; str[y] != '\0'; y++)
-				{
-					_putchar(str[y]);
-					len++;
-				}
-			}	
-			else if (format[x] == '%' && format[x + 1] == '%')
-			{
-				_putchar('%');
-				len++;
-			}
-			else
-			{
-				_putchar(format[x]);
-				len++;
-			}
-		}
-		else
-		{
-			return (-1);
-			len++;
-		}
-	}
-	va_end(arg);
-	return(len);
+    if (str != NULL)
+    {
+        for (x = 0; str[x] != '\0'; x++)
+        {
+            _putchar(str[x]);
+        }
+    }
+    else if (str == NULL)
+    {
+        _putchar(*n);
+    }
 }
+	
