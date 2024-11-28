c309b840018f760566cb2ab547b057870d287fb1
azbin-spec <dydouhabdellah82@gmail.com>
2024-11-28 09:39:42 +0100

    small update

--- percent.c
+++ percent.c
@@ -1,15 +1,40 @@
 #include "main.h"
 
 /**
- * _percent - print %.
- * @args: list of arguments.
- * Return: number of chars.
+ * _int - print integer.
+ * @args: arguments.
+ *
+ * Return: count chars.
  */
-int _percent(va_list args)
+
+int _int(va_list args)
 {
-	(void)args;
+	long int num = va_arg(args, int);
+	int numcount = 0;
+	int mul = 1;
+	long int div;
 
-	_putchar('%');
+	if (num < 0)
+	{
+		numcount += _putchar('-');
+		num *= -1;
+	}
 
-	return (1);
+	if (num < 10)
+		return (numcount += _putchar(num + '0'));
+
+	div = num;
+
+	while (div > 9)
+	{
+		mul *= 10;
+		div /= 10;
+	}
+	while (mul >= 1)
+	{
+		numcount += _putchar(((num / mul) % 10) + '0');
+		mul /= 10;
+	}
+
+	return (numcount);
 }
