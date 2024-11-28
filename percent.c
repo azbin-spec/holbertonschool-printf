f2509cf80a215932ba3700b52ef6e57e24f9ceab
azbin-spec <dydouhabdellah82@gmail.com>
2024-11-28 09:49:33 +0100

    percent.c

--- percent.c
+++ percent.c
@@ -1,40 +1,109 @@
 #include "main.h"
 
 /**
- * _int - print integer.
- * @args: arguments.
+ * print_char - a function that print the character
  *
- * Return: count chars.
+ * @args: argument that will replace the format specifier, if any
+ *
+ * Return: return the function _putchar with the argument as the character
+ *
  */
 
-int _int(va_list args)
+int print_char(va_list args)
 {
-	long int num = va_arg(args, int);
-	int numcount = 0;
-	int mul = 1;
-	long int div;
+	char charac = va_arg(args, int);
+
+	return (_putchar(charac));
+}
+
+/**
+ * print_string - a function that print the string
+ *
+ * @args: argument that will replace the format specifier, if any
+ *
+ * Return: return len of printed string
+ *
+ */
+
+int print_string(va_list args)
+{
+	int len = 0;
+	char *string = va_arg(args, char *);
+
+	if (!string)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (6);
+	}
+
+	while (string[len])
+	{
+		_putchar(string[len]);
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * print_percent - a function that print the percent
+ *
+ * @args: argument that will replace the format specifier, if any
+ *
+ * Return: return number of printed char
+ *
+ */
+
+int print_percent(va_list args)
+{
+	(void)args;
+
+	_putchar('%');
+
+	return (1);
+}
+
+/**
+ * print_integer - a function that print the integer
+ *
+ * @args: argument that will replace the format specifier, if any
+ *
+ * Return: return the number of printed character
+ */
+
+int print_integer(va_list args)
+{
+	long int div, num = va_arg(args, int);
+	int power = 1, result = 0;
 
 	if (num < 0)
 	{
-		numcount += _putchar('-');
+		result += _putchar('-');
 		num *= -1;
+		/*print - char and send num as positive*/
 	}
 
 	if (num < 10)
-		return (numcount += _putchar(num + '0'));
+		return (result += _putchar(num + '0'));
 
 	div = num;
 
 	while (div > 9)
 	{
-		mul *= 10;
+		power *= 10;
 		div /= 10;
 	}
-	while (mul >= 1)
+
+	while (power >= 1)
 	{
-		numcount += _putchar(((num / mul) % 10) + '0');
-		mul /= 10;
+		result += _putchar(((num / power) % 10) + '0');
+		power /= 10;
 	}
 
-	return (numcount);
-}
+	return (result);
+} function
