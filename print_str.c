201944d69b3a9cbcd33dddd65eeb665fc944d2da
azbin-spec <dydouhabdellah82@gmail.com>
2024-11-28 10:32:07 +0100

    print_str.c

--- print_str.c
+++ print_str.c
@@ -7,19 +7,19 @@
 
 void print_str(const char *str)
 {
-    int x;
-    const char *n = (null);
+	int x;
+	const char *n = (null);
 
-    if (str != NULL)
-    {
-        for (x = 0; str[x] != '\0'; x++)
-        {
-            _putchar(str[x]);
-        }
-    }
-    else if (str == NULL)
-    {
-        _putchar(*n);
-    }
+	if (str != NULL)
+	{
+		for (x = 0; str[x] != '\0'; x++)
+		{
+			_putchar(str[x]);
+		}
+	}
+	else if (str == NULL)
+	{
+		_putchar(*n);
+	}
 }
-	
+
