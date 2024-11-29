9b6dbb9f78d133575dc2053c659ab3601f5a254d
Bree-Coding <brendapollagba@gmail.com>
2024-11-29 13:31:44 +0100

    main.h : added %d, i% prototypes

--- main.h
+++ main.h
@@ -10,7 +10,6 @@ int _printf(const char *format, ...);
 void print_char(char character);
 void print_str(const char *str);
 void print_modulo(void);
-int print_integer(int num);
-int print_negative(int num);
+int print_integer(long int num);
 
 #endif
