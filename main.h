e34b5ed6896fa68324661c2fbc3981901dfea4ab
Bree-Coding <brendapollagba@gmail.com>
2024-11-29 17:16:43 +0100

    modified the main.h with int functions so it can return the length and added the structure

--- main.h
+++ main.h
@@ -7,9 +7,23 @@
 
 char _putchar(char character);
 int _printf(const char *format, ...);
-void print_char(char character);
-void print_str(const char *str);
-void print_modulo(void);
-int print_integer(long int num);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_modulo(va_list args);
+int print_integer(va_list args);
+int get_specifier(const char format, va_list args);
+
+/**
+ * struct spe - a structure that check specifier
+ * @specifier: the character specifier
+ * @f: a function
+ */
+
+typedef struct spe
+{
+	char specifier;
+	int (*f)(va_list);
+
+} specifier_t;
 
 #endif
